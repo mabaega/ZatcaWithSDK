60106381421765abff759fca4d105ca9db24266a
mabaega <mabaega@gmail.com>
2024-09-22 19:10:50 +0700

    Fix warning KSA 05

---
+++ SDKValidation/Program.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Xml;
+using Zatca.EInvoice.SDK;
+using Zatca.EInvoice.SDK.Contracts.Models;
+
+
+namespace SDKValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ObboardingJsonPath = @"..\..\..\Data\MyCertificate\ObboardingInfo.json";
+            var CleanXmlPath = @"..\..\..\Data\InvSample\CleanSimplified_Invoice.xml";
+            var SignedXmlPath = @"..\..\..\Data\InvSample\Signed_Simplified_Invoice.xml";
+
+            OnboardingResult onboardingResult = DeserializeFromFile(ObboardingJsonPath);
+
+            XmlDocument document = new() { PreserveWhitespace = true };
+            document.Load(CleanXmlPath);
+
+            string PCSID_Certificate = Encoding.UTF8.GetString(Convert.FromBase64String(onboardingResult.PCSIDBinaryToken));
+            SignResult signResult = new EInvoiceSigner().SignDocument(document, PCSID_Certificate, onboardingResult.PrivateKey);
+            if (signResult.IsValid)
+            {
+                Console.WriteLine("Signing Invoice : " + signResult.IsValid);
+                signResult.SaveSignedEInvoice(SignedXmlPath);
+                Console.WriteLine("Signed file parh : "  + SignedXmlPath);
+            }
+            else {
+                foreach (var r in signResult.Steps)
+                {
+                    Console.WriteLine(r.StepName);
+                    Console.WriteLine(r.IsValid);
+                    
+                    foreach (var w in r.ErrorMessages)
+                    {
+                        Console.WriteLine(w.ToString());
+                    }
+
+                    foreach (var e in r.ErrorMessages)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+        public static OnboardingResult DeserializeFromFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<OnboardingResult>(json);
+        }
+
+        public class OnboardingResult
+        {
+            public string GeneratedCsr { get; set; }
+            public string PrivateKey { get; set; }
+            public string CCSIDComplianceRequestId { get; set; }
+            public string CCSIDBinaryToken { get; set; }
+            public string CCSIDSecret { get; set; }
+            public string PCSIDBinaryToken { get; set; }
+            public string PCSIDSecret { get; set; }
+        }
+    }
+}
+
+
+////Test eInvoice SDK Validator
+//string cert = Encoding.UTF8.GetString(Convert.FromBase64String(certificateInfo.PCSIDBinaryToken));
+//XmlDocument zdocument = new XmlDocument() { PreserveWhitespace = true };
+//zdocument.LoadXml(Encoding.UTF8.GetString(Convert.FromBase64String(requestResult.InvoiceRequest.Invoice)));
+
+//Zatca.EInvoice.SDK.Contracts.Models.ValidationResult result = new EInvoiceValidator().ValidateEInvoice(zdocument, cert, pihforvalidation);
+//foreach (var v in result.ValidationSteps)
+//{
+//    Console.WriteLine(v.ValidationStepName);
+//    Console.WriteLine(v.IsValid);
+//    foreach (var v2 in v.ErrorMessages)
+//    {
+//        Console.WriteLine(v2.ToString());
+//    }
+
+//    Console.WriteLine();
+//}
--- ZatcaWithSDK/Program.cs
+++ ZatcaWithSDK/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 using System.Xml;
-using Zatca.EInvoice.SDK.Contracts.Models;
+using Zatca.EInvoice.SDK;
 using static ZatcaWithSDK.Models;
 
 namespace ZatcaWithSDK
@@ -15,33 +16,33 @@ namespace ZatcaWithSDK
 
             try
             {
-                //Onboarding
-                Console.WriteLine($"\nI. ONBOARDING PROCESS\n\n");  //comented this line when first onboarding success
-                var onboardingResult = await zatcaService.OnboardingDevice();  //comented this line when first onboarding success
-
-                //Save OnboardingInfo
                 var ObboardingJsonPath = @"..\..\..\Data\MyCertificate\ObboardingInfo.json";
+
+
+                //Commented this block after 1st run with successfully onboarding
+                #region "Onboarding" 
+
+                Console.WriteLine($"\nI. ONBOARDING PROCESS\n\n");
+                var onboardingResult = await zatcaService.OnboardingDevice();
+                //Save OnboardingInfo
                 Helpers.SerializeToFile(onboardingResult, ObboardingJsonPath);
 
-                // onboarding step is done here, we need to safe all onboarding info to use on approval invoice process
-               
-                //=================================                
+                #endregion
 
 
                 Console.WriteLine($"\n\nII. APPROVAL PROCESS\n\n");
-                //Send Invoice
 
-                XmlDocument document = new XmlDocument() { PreserveWhitespace = true };
+                //Load CertificateInfo
+                OnboardingResult certificateInfo = Helpers.DeserializeFromFile(ObboardingJsonPath);
+
+                XmlDocument document = new() { PreserveWhitespace = true };
                 document.Load(@"..\..\..\Data\InvSample\CleanSimplified_Invoice.xml");
-
-                //Load OnboardingInfo
-                onboardingResult = Helpers.DeserializeFromFile(ObboardingJsonPath);
 
                 XmlDocument newDoc;
 
                 var ICV = "0";
                 var PIH = "NWZlY2ViNjZmZmM4NmYzOGQ5NTI3ODZjNmQ2OTZjNzljMmRiYzIzOWRkNGU5MWI0NjcyOWQ3M2EyN2ZiNTdlOQ==";
-                RequestResult requestResult;
+                Zatca.EInvoice.SDK.Contracts.Models.RequestResult requestResult;
                 ServerResult serverResult;
 
                 var settings = new JsonSerializerSettings
@@ -54,8 +55,8 @@ namespace ZatcaWithSDK
                 //Standard Invoice
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "STDSI-001", new Guid().ToString(), "0100000", "388", ICV, PIH, "");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, true);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, true);
                 if (serverResult != null && !serverResult.ClearanceStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
@@ -65,8 +66,8 @@ namespace ZatcaWithSDK
                 //Standard CreditNote
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "STDCN-001", new Guid().ToString(), "0100000", "383", ICV, PIH, "Standard CreditNote");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, true);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, true);
                 if (serverResult != null && !serverResult.ClearanceStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
@@ -75,8 +76,8 @@ namespace ZatcaWithSDK
                 //Standard DebitNote
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "STDDN-001", new Guid().ToString(), "0100000", "381", ICV, PIH, "Standard DebitNote");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, true);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, true);
                 if (serverResult != null && !serverResult.ClearanceStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
@@ -89,35 +90,61 @@ namespace ZatcaWithSDK
                 //simplified Invoice
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "SIMSI-001", new Guid().ToString(), "0200000", "388", ICV, PIH, "");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, false);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, false);
                 if (serverResult != null && !serverResult.ReportingStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
                 }
                 Console.WriteLine($"Reporting Simplified Invoice \n{JsonConvert.SerializeObject(serverResult, Newtonsoft.Json.Formatting.Indented, settings)}\n\n");
+
                 //simplified CreditNote
+                string pihforvalidation = string.Empty;
+
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "SIMCN-001", new Guid().ToString(), "0200000", "383", ICV, PIH, "simplified CreditNote");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, false);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, false);
                 if (serverResult != null && !serverResult.ReportingStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
+                    pihforvalidation = PIH;
                 }
                 Console.WriteLine($"Reporting Simplified Credit Note \n{JsonConvert.SerializeObject(serverResult, Newtonsoft.Json.Formatting.Indented, settings)}\n\n");
+
                 //simplified DebitNote
                 ICV += ICV;
                 newDoc = Helpers.CreateModifiedInvoiceXml(document, "SIMDN-001", new Guid().ToString(), "0200000", "381", ICV, PIH, "simplified DebitNote");
-                requestResult = Helpers.GenerateSignedRequestApi(newDoc, onboardingResult.PCSIDBinaryToken, onboardingResult.PrivateKey);
-                serverResult = await Helpers.GetApproval(onboardingResult.PCSIDBinaryToken, onboardingResult.PCSIDSecret, requestResult.InvoiceRequest, false);
+                requestResult = Helpers.GenerateSignedRequestApi(newDoc, certificateInfo.PCSIDBinaryToken, certificateInfo.PrivateKey);
+                serverResult = await Helpers.GetApproval(certificateInfo.PCSIDBinaryToken, certificateInfo.PCSIDSecret, requestResult.InvoiceRequest, false);
                 if (serverResult != null && !serverResult.ReportingStatus.Contains("NOT"))
                 {
                     PIH = requestResult.InvoiceRequest.InvoiceHash;
+
                 }
                 Console.WriteLine($"Reporting Simplified Debit Note \n{JsonConvert.SerializeObject(serverResult, Newtonsoft.Json.Formatting.Indented, settings)}\n\n");
 
                 Console.WriteLine($"\n\nALL DONE!\n\n");
+
+
+                ////Test eInvoice SDK Validator
+                //string cert = Encoding.UTF8.GetString(Convert.FromBase64String(certificateInfo.PCSIDBinaryToken));
+                //XmlDocument zdocument = new XmlDocument() { PreserveWhitespace = true };
+                //zdocument.LoadXml(Encoding.UTF8.GetString(Convert.FromBase64String(requestResult.InvoiceRequest.Invoice)));
+
+                //Zatca.EInvoice.SDK.Contracts.Models.ValidationResult result = new EInvoiceValidator().ValidateEInvoice(zdocument, cert, pihforvalidation);
+                //foreach (var v in result.ValidationSteps)
+                //{
+                //    Console.WriteLine(v.ValidationStepName);
+                //    Console.WriteLine(v.IsValid);
+                //    foreach (var v2 in v.ErrorMessages)
+                //    {
+                //        Console.WriteLine(v2.ToString());
+                //    }
+
+                //    Console.WriteLine();
+                //}
+
             }
             catch (Exception ex)
             {
